773365155c7a6a8a5c0bbca1419a09c1c2c1d0d6
Teja <saiteja7171@gmail.com>
2025-02-21 17:57:55 -0600

    Initial Commit

---
+++ GitLearning/GitLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitLearningApp.swift
+//  GitLearning
+//
+//  Created by Sai Teja Atluri on 2/21/25.
+//
+
+import SwiftUI
+
+@main
+struct GitLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitLearningTests/GitLearningTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitLearningTests.swift
+//  GitLearningTests
+//
+//  Created by Sai Teja Atluri on 2/21/25.
+//
+
+import Testing
+@testable import GitLearning
+
+struct GitLearningTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
