90073f3c87c41f49a8c9f6ee08dac443fbc8c960
Teja <saiteja7171@gmail.com>
2025-02-21 18:11:58 -0600

    [Feature] Added a button "Click me"

--- GitLearning/ContentView.swift
+++ GitLearning/ContentView.swift
@@ -11,6 +11,16 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("I am learning how to master GIT")
+            Button("Click me") {
+                
+            }
+            .frame(width: 200, height: 50)
+            .font(.title)
+            .background(Color.blue)
+            .tint(Color.white)
+            .cornerRadius(10)
+            .padding()
+            
         }
         .padding()
     }
